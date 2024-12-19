d3c8cb4a90b995a4f373a23b7009a2bdccc3fc15
IvanTusanin <ivantusanin@gmail.com>
2024-12-19 16:05:58 +0300

    ADD1

--- for_homework_PS06.py
+++ for_homework_PS06.py
@@ -1,33 +1,59 @@
+
 import time
 import csv
-from os import write
-
-from django.template.defaultfilters import title, pprint
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.devtools.v85.fetch import continue_request
-from urllib3.filepost import writer
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import TimeoutException
 
+# Инициализация веб-драйвера
 driver = webdriver.Chrome()
 
-url = ("https://www.ststroitel.ru/catalog/osveshchenie/lampy_svetodiodnye/")
+# URL для парсинга
+url = "https://spb.hh.ru/vacancies/programmist"
 driver.get(url)
-time.sleep(30)
-lamps = driver.find_elements(By.CSS_SELECTOR, "top-block-wrapper")
-parsed_data = []
-for lamp in lamps:
-    try:
-        title = lamp.find_elements(By.CSS_SELECTOR, "span.Ленты светодиодные").text
-        company = lamp.find_elements(By.CSS_SELECTOR,"topic__heading").text
 
-        price = lamp.find_elements(By.CSS_SELECTOR, "price_currency").text
-        link = lamp.find_elements(By.CSS_SELECTOR, "a.scroll-to-top ROUND_COLOR PADDING" ).get_attribute("href")
-    except:
-        print("произошла ошибка при парсинге")
-        continue
-    parsed_data.append([title, company, price, link])
+# Использование WebDriverWait для ожидания загрузки вакансий
+try:
+    element = WebDriverWait(driver, 10).until(
+        EC.presence_of_element_located((By.CSS_SELECTOR, "a.magritte-link")))
+
+except TimeoutException:
+    print("Элемент не найден на странице")
     driver.quit()
-with open("ststroitel.csv", 'w',newline='', encoding='utf-8') as file:
+    exit()
+
+# Находим все вакансии
+vacancies = driver.find_elements(By.CLASS_NAME, "vacancy-info--ieHKDTkezpEj0Gsx")
+
+parsed_data = []
+
+# Парсинг информации о вакансиях
+for vacancy in vacancies:
+    try:
+        title_element = vacancy.find_element(By.CSS_SELECTOR, ".vacancy-name-wrapper--PSD41i3dJDUNb5Tr")
+        title = title_element.text
+
+        company_element = vacancy.find_element(By.CSS_SELECTOR, ".company-name-badges-container--ofqQHaTYRFg0JM18")
+        company = company_element.text
+
+        salary_element = vacancy.find_element(By.CSS_SELECTOR, ".wide-container-magritte--TmgLMNxG7w8MIgQd")
+        salary = salary_element.text
+
+        link_element = vacancy.find_element(By.CSS_SELECTOR, "a.""magritte-link___b4rEM_4-3-14 ").text
+
+    except Exception as e:
+        print(f"Произошла ошибка при парсинге: {e}")
+        continue
+
+    parsed_data.append([title, company, salary, link])
+
+# Закрываем драйвер
+driver.quit()
+
+# Сохраняем данные в CSV файл
+with open("hh.csv", 'w', newline='', encoding='utf-8') as file:
     writer = csv.writer(file)
-    writer.writerow(["Название", "Производитель", "Цена", "Ссылка"])
+    writer.writerow(['Название вакансии', 'Название компании', 'Зарплата', 'Ссылка на вакансию'])
     writer.writerows(parsed_data)
